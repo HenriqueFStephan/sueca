440a88ac1eb29f20bdad9890f308aee8aba9aa51
Henrique F. Stephan <henrique.f.stephan@gmail.com>
2023-03-13 14:11:33 -0300

    first commit

---
+++ Carta/index.js
@@ -0,0 +1,148 @@
+ export class Carta{
+
+    static possibleValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'Joker'];
+    static possibleNipes = ['♤', '♡', '♢', '♧'];
+
+    constructor(nipe, value, id){
+        this.nipe = nipe
+        this.value = value
+        this.id = `${this.value}${this.nipe}`
+    }
+
+    get nipeCarta() {
+        return this.nipe
+    }
+
+    get valueCarta() {
+        return this.value
+    }
+
+    get idCarta() {
+        return this.id
+    }
+
+    set nipeCarta(nipeCarta) {
+        this.nipe = nipeCarta
+    }
+
+    set valueCarta(valueCarta) {
+        this.value = valueCarta
+    }
+
+    set idCarta(idCarta) {
+        this.id = idCarta
+    }
+
+
+    randomCard() {
+        const v = Carta.possibleValues[Math.floor(Math.random()*Carta.possibleValues.length)]
+        const n = (v === 'Joker') ? '' : Carta.possibleNipes[Math.floor(Math.random() * Carta.possibleNipes.length)];
+        const interpoler = Math.floor(Math.random()*1000)
+        this.value = v
+        this.nipe = n
+        this.id = `${v}${n}${interpoler}`
+    }
+
+    displayCard(local, height, backSideUp=false) {
+        const parent = document.getElementById(local)
+        const container = document.createElement('div')
+        parent.style.perspective = '400px'
+        container.style.height = `${height}`
+        container.style.width = `calc(${height}/1.56)`
+        container.style.position = 'absolute'
+        container.style.transformStyle = 'preserve-3d'
+        container.classList.add('displayedCard')
+
+        const frontSide = document.createElement('div')
+        frontSide.setAttribute('class', 'cardLimits')
+        frontSide.setAttribute('id', `${this.id}`)
+        frontSide.setAttribute('style', `display: flex;
+                                    flex-direction: column;
+                                    background-color: rgb(40, 40, 40);
+                                    height:${height};
+                                    width:calc(${height}/1.56);
+                                    border-radius: 5%;
+                                    color: rgb(187, 194, 204);
+                                    justify-content: space-around;
+                                    align-items: center;
+                                    font-size: calc(${height}/7.5);
+                                    box-shadow: calc(${height}/100) calc(${height}/200) calc(${height}/20) calc(${height}/80) #888888;
+                                    position: absolute;
+                                    backface-visibility: hidden;`)
+        const valueUp = document.createElement('p')
+        valueUp.setAttribute('class', 'cardValue')
+        valueUp.innerHTML= `${this.value}`
+        const valueDown = document.createElement('p')
+        valueDown.setAttribute('class', 'cardDown')
+        valueDown.setAttribute('style', 'rotate: 180deg')
+        valueDown.innerHTML = `${this.value}`
+        const inner = document.createElement('div')
+        inner.setAttribute('class', 'nipe')
+        inner.setAttribute('style', `display: flex;
+                                    height: calc(${height}*0.63);
+                                    width: calc(${height}*0.40);
+                                    border: calc(${height}/5) rgb(187, 194, 204) solid;
+                                    align-items: center;
+                                    justify-content: center;
+                                    border-width: calc(${height}/150) 0px calc(${height}/150) 0px;
+                                    font-size: calc(${height}/3);
+                                    border-radius: calc(${height}/30);
+                                    background-color: rgb(50, 50, 50);`) //deixar com cor diferente?                                    
+        inner.innerHTML = `${this.nipe}`
+        frontSide.appendChild(valueUp)
+        frontSide.appendChild(inner)
+        frontSide.appendChild(valueDown)
+        const backSide = document.createElement('div')
+        backSide.style.backgroundColor = 'rgb(50, 50, 50)'
+        backSide.style.display = 'flex'
+        backSide.style.flexDirection = 'column'
+        backSide.style.justifyContent = 'center'
+        backSide.style.alignContent = 'center'
+        backSide.style.position = 'absolute'
+        backSide.innerHTML = 'JOGOS DA MANGA'
+        backSide.style.position = 'absolute'
+        backSide.style.height =  `${height}`
+        backSide.style.width =  `calc(${height}/1.56)`
+        backSide.style.backfaceVisibility = 'hidden'
+        backSide.style.color = 'RGB(200,200,200)'
+        //backSide.style.border = '0.01px solid white' Deixar melhor
+        backSide.style.borderRadius = '5%'
+        backSide.style.textAlign = 'center'
+        backSide.setAttribute('class', 'backSide')
+        parent.appendChild(container)
+        container.appendChild(frontSide)
+        container.appendChild(backSide)
+        if (backSideUp) {
+            frontSide.style.transform = 'rotateY(180deg)'
+        }else {
+            backSide.style.transform = 'rotateY(180deg)'
+        }
+    }
+
+    moveRotateCard(x, y, z, rotateX, rotateY, rotateZ, scale, time) {
+        return new Promise((resolve) => {
+          let card = document.getElementById(`${this.id}`)
+          card.parentElement.style.transition = `all ${time}`
+      
+          for (let i = 0; i < 10; i++) {
+            setTimeout(() => {
+              card.parentElement.style.transform = `translateX(${x}) translateY(${y}) translateZ(${z}) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg) scale(${scale})`
+              if (i === 9) {
+                resolve()
+              }
+            }, i * 100)
+          }
+        })
+      }
+      
+
+    moveRotateCardPreset(x, y, z, rotateX, rotateY, rotateZ, scale, time) {
+        let card = document.getElementById(`${this.id}`)
+        card.parentElement.style.transition = `all ${time}`
+        card.parentElement.style.transform = `translateX(${x}) translateY(${y}) translateZ(${z}) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg) scale(${scale})`
+
+    }
+}
+
+
+
--- index.js
+++ index.js
@@ -1,27 +1,56 @@
+import { Carta } from "./Carta/index.js"
 
 class Deck {
 
     constructor(deck, usedCards){
-        this.deck = [['a', '&#9829'], ['2', '&#9829'], ['3', '&#9829'], ['4', '&#9829'], ['5', '&#9829'], ['6', '&#9829'], ['7', '&#9829'], ['8', '&#9829'], ['9', '&#9829'], ['10', '&#9829'], ['j', '&#9829'], ['q', '&#9829'], ['k', '&#9829'],
-        ['a', '&#9824'], ['2', '&#9824'], ['3', '&#9824'], ['4', '&#9824'], ['5', '&#9824'], ['6', '&#9824'], ['7', '&#9824'], ['8', '&#9824'], ['9', '&#9824'], ['10', '&#9824'], ['j', '&#9824'], ['q', '&#9824'], ['k', '&#9824'],
-        ['a', '&#9830'], ['2', '&#9830'], ['3', '&#9830'], ['4', '&#9830'], ['5', '&#9830'], ['6', '&#9830'], ['7', '&#9830'], ['8', '&#9830'], ['9', '&#9830'], ['10', '&#9830'], ['j', '&#9830'], ['q', '&#9830'], ['k', '&#9830'],
-        ['a', '&#9827'], ['2', '&#9827'], ['3', '&#9827'], ['4', '&#9827'], ['5', '&#9827'], ['6', '&#9827'], ['7', '&#9827'], ['8', '&#9827'], ['9', '&#9827'], ['10', '&#9827'], ['j', '&#9827'], ['q', '&#9827'], ['k', '&#9827'],
-        'joker', 'joker'];
-        this.usedCards = [];
-    }
-    resetDeck() {
-        this.deck = [['a', '&#9829'], ['2', '&#9829'], ['3', '&#9829'], ['4', '&#9829'], ['5', '&#9829'], ['6', '&#9829'], ['7', '&#9829'], ['8', '&#9829'], ['9', '&#9829'], ['10', '&#9829'], ['j', '&#9829'], ['q', '&#9829'], ['k', '&#9829'],
-        ['a', '&#9824'], ['2', '&#9824'], ['3', '&#9824'], ['4', '&#9824'], ['5', '&#9824'], ['6', '&#9824'], ['7', '&#9824'], ['8', '&#9824'], ['9', '&#9824'], ['10', '&#9824'], ['j', '&#9824'], ['q', '&#9824'], ['k', '&#9824'],
-        ['a', '&#9830'], ['2', '&#9830'], ['3', '&#9830'], ['4', '&#9830'], ['5', '&#9830'], ['6', '&#9830'], ['7', '&#9830'], ['8', '&#9830'], ['9', '&#9830'], ['10', '&#9830'], ['j', '&#9830'], ['q', '&#9830'], ['k', '&#9830'],
-        ['a', '&#9827'], ['2', '&#9827'], ['3', '&#9827'], ['4', '&#9827'], ['5', '&#9827'], ['6', '&#9827'], ['7', '&#9827'], ['8', '&#9827'], ['9', '&#9827'], ['10', '&#9827'], ['j', '&#9827'], ['q', '&#9827'], ['k', '&#9827'],
-        'joker', 'joker'];
+        this.deck = [['A', '♤'], ['2', '♤'], ['3', '♤'], ['4', '♤'], ['5', '♤'], ['6', '♤'], ['7', '♤'], ['8', '♤'], ['9', '♤'], ['10', '♤'], ['J', '♤'], ['Q', '♤'], ['K', '♤'],
+        ['A', '♡'], ['2', '♡'], ['3', '♡'], ['4', '♡'], ['5', '♡'], ['6', '♡'], ['7', '♡'], ['8', '♡'], ['9', '♡'], ['10', '♡'], ['J', '♡'], ['Q', '♡'], ['K', '♡'],
+        ['A', '♢'], ['2', '♢'], ['3', '♢'], ['4', '♢'], ['5', '♢'], ['6', '♢'], ['7', '♢'], ['8', '♢'], ['9', '♢'], ['10', '♢'], ['J', '♢'], ['Q', '♢'], ['K', '♢'],
+        ['A', '♧'], ['2', '♧'], ['3', '♧'], ['4', '♧'], ['5', '♧'], ['6', '♧'], ['7', '♧'], ['8', '♧'], ['9', '♧'], ['10', '♧'], ['J', '♧'], ['Q', '♧'], ['K', '♧'],
+        ['Joker', ''], ['Joker', '']];
         this.usedCards = [];
     }
 
+    resetDeck() {
+        this.deck = [['A', '♤'], ['2', '♤'], ['3', '♤'], ['4', '♤'], ['5', '♤'], ['6', '♤'], ['7', '♤'], ['8', '♤'], ['9', '♤'], ['10', '♤'], ['J', '♤'], ['Q', '♤'], ['K', '♤'],
+        ['A', '♡'], ['2', '♡'], ['3', '♡'], ['4', '♡'], ['5', '♡'], ['6', '♡'], ['7', '♡'], ['8', '♡'], ['9', '♡'], ['10', '♡'], ['J', '♡'], ['Q', '♡'], ['K', '♡'],
+        ['A', '♢'], ['2', '♢'], ['3', '♢'], ['4', '♢'], ['5', '♢'], ['6', '♢'], ['7', '♢'], ['8', '♢'], ['9', '♢'], ['10', '♢'], ['J', '♢'], ['Q', '♢'], ['K', '♢'],
+        ['A', '♧'], ['2', '♧'], ['3', '♧'], ['4', '♧'], ['5', '♧'], ['6', '♧'], ['7', '♧'], ['8', '♧'], ['9', '♧'], ['10', '♧'], ['J', '♧'], ['Q', '♧'], ['K', '♧'],
+        ['Joker', ''], ['Joker', '']];
+        this.usedCards = [];
+    }
+    placeDeck(local, height, random, downSideUp, entrie=0) {
+        if (!random){
+            for (let i = 0; i < 54; i++) {
+                let card = new Carta(this.deck[i][1], this.deck[i][0], i, downSideUp);
+                card.displayCard(local, height, true);
+                card.moveRotateCardPreset(`calc(${i}px/10)`, `calc(-${i}px/5)`, 0, 15, 0, 0, 1, 0);
+                entrie += 1
+            }
+        }else {
+            for (let index in this.deck) {
+                let card = new Carta
+                card.randomCard()
+                if (this.deck[index].includes(card.value)) {
+                    if (this.deck[index].includes(card.nipe)) {
+                        this.deck.splice(index, 1)
+                        card.displayCard(local, height, true, downSideUp=true);
+                        card.moveRotateCardPreset(`calc(0.2*${entrie}px)`, `calc(-0.2*${entrie}px)`, 0, 5, 0, 0, 1, '1s')
+                        entrie += 1
+                    }
+                }
+                else {
+                    this.placeDeck(local, height, true, downSideUp, entrie)
+                }  
+            }    
+        }
+    }
+
+    
     draw() {
         buttom.innerHTML = 'draw'
         if (this.deck.length >= 1) {
-            let card = new Card;
+            let card = new Carta;
             let prosseguir = false;
             card.random();
             for (let i in this.deck) {
@@ -67,101 +96,12 @@ class Deck {
         };
 }
 
-class Card {
-    valoresPossiveis = ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'joker'];
-    nipesPossiveis = ['&#9829', '&#9824', '&#9830', '&#9827'];
-    valor;
-    nipe;
-    constructor(valor, nipe) {
-        this.valor = valor;
-        this.nipe = nipe;
-        this.valoresPossiveis = this.valoresPossiveis;
-        this.nipesPossiveis = this.nipesPossiveis;
-    }
-    random() {
-        this.valor = choose(this.valoresPossiveis);
-        if (this.valor == 'joker') {
-            return this.valor;
-        }
-        else {
-            this.nipe = choose(this.nipesPossiveis);
-            return [this.valor, this.nipe];
-        }
-    }
-
-    displayCard(value, nipe) {
-        this.innerTemplate(value, nipe, 'main')
-        }
-        
-    innerTemplate(value, nipe, local) {
-        let body = document.getElementById(`${local}`);
-        switch (value) {
-            case 'a':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="templateA5329" class="template"><div class="a" style="visibility: hidden;">${nipe}</div><div class="b" style="visibility: hidden;">${nipe}</div><div class="c" style="visibility: hidden;">${nipe}</div><div class="d" style="visibility: hidden;">${nipe}</div><div class="e" style="visibility: hidden;">${nipe}</div><div class="f" style="visibility: hidden;">${nipe}</div><div class="g" style="visibility: hidden;">${nipe}</div><div class="h" style="visibility: hidden;">${nipe}</div><div class="i" style="visibility: hidden;">${nipe}</div><div class="j" style="visibility: hidden;">${nipe}</div><div class="k">${nipe}</div></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case '2':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="templateA5329" class="template"><div class="a" style="visibility: hidden;">${nipe}</div><div class="b" style="visibility: hidden;">${nipe}</div><div class="c" style="visibility: hidden;">${nipe}</div><div class="d" style="visibility: hidden;">${nipe}</div><div class="e" style="visibility: hidden;">${nipe}</div><div class="f" style="visibility: hidden;">${nipe}</div><div class="g" style="visibility: hidden;">${nipe}</div><div class="h" style="visibility: hidden;">${nipe}</div><div class="i">${nipe}</div><div class="j">${nipe}</div><div class="k" style="visibility: hidden;">${nipe}</div></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case '3':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="templateA5329" class="template"><div class="a" style="visibility: hidden;">${nipe}</div><div class="b" style="visibility: hidden;">${nipe}</div><div class="c" style="visibility: hidden;">${nipe}</div><div class="d" style="visibility: hidden;">${nipe}</div><div class="e" style="visibility: hidden;">${nipe}</div><div class="f" style="visibility: hidden;">${nipe}</div><div class="g" style="visibility: hidden;">${nipe}</div><div class="h" style="visibility: hidden;">${nipe}</div><div class="i">${nipe}</div><div class="j">${nipe}</div><div class="k">${nipe}</div></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case '4':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="template4678" class="template"><div class="a">${nipe}</div><div class="b">${nipe}</div><div class="c" style="visibility: hidden;">${nipe}</div><div class="d" style="visibility: hidden;">${nipe}</div><div class="e">${nipe}</div><div class="f">${nipe}</div><div class="i" style="visibility: hidden;">${nipe}</div><div class="j" style="visibility: hidden;">${nipe}</div><div class="k" style="visibility: hidden;">${nipe}</div><div class="l" style="visibility: hidden;">${nipe}</div></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case '5':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="templateA5329" class="template"><div class="a">${nipe}</div><div class="b">${nipe}</div><div class="c" style="visibility: hidden;">${nipe}</div><div class="d" style="visibility: hidden;">${nipe}</div><div class="e" style="visibility: hidden;">${nipe}</div><div class="f" style="visibility: hidden;">${nipe}</div><div class="g">${nipe}</div><div class="h">${nipe}</div><div class="i" style="visibility: hidden;">${nipe}</div><div class="j" style="visibility: hidden;">${nipe}</div><div class="k">${nipe}</div></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case '6':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="template4678" class="template"><div class="a">${nipe}</div><div class="b">${nipe}</div><div class="c">${nipe}</div><div class="d">${nipe}</div><div class="e">${nipe}</div><div class="f">${nipe}</div><div class="i" style="visibility: hidden;">${nipe}</div><div class="j" style="visibility: hidden;">${nipe}</div><div class="k" style="visibility: hidden;">${nipe}</div><div class="l" style="visibility: hidden;">${nipe}</div></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case '7':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="template4678" class="template"><div class="a">${nipe}</div><div class="b">${nipe}</div><div class="c">${nipe}</div><div class="d">${nipe}</div><div class="e">${nipe}</div><div class="f">${nipe}</div><div class="i" style="visibility: hidden;">${nipe}</div><div class="j" style="visibility: hidden;">${nipe}</div><div class="k">${nipe}</div><div class="l" style="visibility: hidden;">${nipe}</div></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case '8':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="template4678" class="template"><div class="a">${nipe}</div><div class="b">${nipe}</div><div class="c">${nipe}</div><div class="d">${nipe}</div><div class="e">${nipe}</div><div class="f">${nipe}</div><div class="i" style="visibility: hidden;">${nipe}</div><div class="j" style="visibility: hidden;">${nipe}</div><div class="k">${nipe}</div><div class="l">${nipe}</div></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case '9':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="template105" class="template"><div class="a">${nipe}</div><div class="b">${nipe}</div><div class="c">${nipe}</div><div class="d">${nipe}</div><div class="e">${nipe}</div><div class="f">${nipe}</div><div class="g">${nipe}</div><div class="h">${nipe}</div><div class="i">${nipe}</div><div class="j" style="visibility: hidden;">${nipe}</div></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case '10':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="template105" class="template"><div class="a">${nipe}</div><div class="b">${nipe}</div><div class="c">${nipe}</div><div class="d">${nipe}</div><div class="e">${nipe}</div><div class="f">${nipe}</div><div class="g">${nipe}</div><div class="h">${nipe}</div><div class="i">${nipe}</div><div class="j">${nipe}</div></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case 'j':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="templateJ" class="template"></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case 'q':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="templateQ" class="template"></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case 'k':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"><p id="pValue">${value}</p><p id="nipe">${nipe}</p></div><div id="templateK" class="template"></div><div id="outerLetterEnd"><p>${value}</p><p id="nipe">${nipe}</p></div></div>`
-                break;
-            case 'joker':
-                body.innerHTML = `<div id="outerTemplate"><div id="outerLetter"></div><div id="templateJoker" class="template"></div><div id="outerLetterEnd"></div></div>`
-                break;
-        }
-    }
-}
-
 function choose(choices) {
     var index = Math.floor(Math.random() * choices.length);
     return choices[index];
   }
 
-let deck = new Deck;
-let card = new Card;
-
-const buttom = document.getElementById('draw');
-
-buttom.addEventListener('click', function() {
-    deck.draw();
-    if (buttom.innerHTML == 'Shuffle') {
-        deck.resetDeck();
-    }
-});
-
-
-
-const regras = {'a': '1 shot',
+const regras = {'A': '1 shot',
                 '2': '2 shots',
                 '3': '3 shots',
                 '4': 'Verdade, Desafio ou Shot',
@@ -171,10 +111,10 @@ const regras = {'a': '1 shot',
                 '8': 'Espelho',
                 '9': 'Tirar regra',
                 '10': 'Jogo do pi',
-                'j': 'Homens bebem',
-                'q': 'Mulheres bebem',
-                'k': 'Não-Binários bebem',
-                'joker': 'SE FUDEU, beba 2 shots'}
+                'J': 'Homens bebem',
+                'Q': 'Mulheres bebem',
+                'K': 'Não-Binários bebem',
+                'Joker': 'SE FUDEU, beba 2 shots'}
 
 const regrasLongas = {'a': 'O jogador que tirou a carta escolhe 1 pessoa para dar um shot',
                       '2': 'O jogador que tirou a carta distribui 2 shots entre os participantes',
@@ -188,18 +128,49 @@ const regrasLongas = {'a': 'O jogador que tirou a carta escolhe 1 pessoa para da
                       '10': 'Jogue uma rodada do "Jogo do pi", o perdedor bebe',
                       'j': 'Todos os homens bebem',
                       'q': 'Todas as mulheres bebem',
-                      'k': 'todos os não-binários bebem',
+                      'k': 'Todos os não-binários bebem',
                       'joker': 'Já ta explicado'}
 
+                      
+let regra = document.createElement('p');
+regra.setAttribute('class', 'regra');
+regra.setAttribute('id', 'regra');
 function exibiRegra(value) {
-    let regra = document.createElement('p');
-    regra.setAttribute('class', 'regra');
     regra.innerHTML = `${regras[value]}`;
     let local = document.getElementById('main');
     local.appendChild(regra);
-    let n = 1
     regra.addEventListener('click', function() {
         regra.innerHTML = `${regrasLongas[value]}`;
     })
 }
+
+
+function handleClick(event) {
+    var clickedElement = event.target.closest('.displayedCard');
+    var previousCard = clickedElement.nextSibling;
+    let value = clickedElement.firstChild.firstChild.innerHTML
+    console.log(clickedElement);
+    clickedElement.style.transition = '1s all'
+    if (!previousCard){
+        clickedElement.style.transform = `translateY(-200px) rotateY(180deg) scale(2)`
+    }
+    else {
+        previousCard.style.transition = '1s all'
+        clickedElement.style.transform = `translateY(-200px) rotateY(180deg) scale(2)`
+        previousCard.style.transform = `translateY(-200px) translateX(800px) rotateY(180deg) scale(2)`
+    }
+    exibiRegra(value)
+
+}
+
+// Add the click event listeners to the document
+document.addEventListener("click", handleClick);
+
+
+let deck = new Deck
+deck.placeDeck('cardSlot', '20vh', true, true)
+
+
+
+
 
